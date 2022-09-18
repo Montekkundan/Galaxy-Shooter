c4e9c6630c56da3167be03d9734d59677ddad51e
Montek <montekkundan@gmail.com>
2022-09-18 10:14:23 +0530

    Added shooting laser

--- Galaxy-Shooter/Assets/Scripts/Player.cs
+++ Galaxy-Shooter/Assets/Scripts/Player.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 3.5f;
+    private float _speed = 7.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.15f;
+    private float _canFire = -1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +21,10 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
-
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            ShootLaser(); 
+        }
     }
     void CalculateMovement()
     {
@@ -34,5 +42,10 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
+    }
+    void ShootLaser()
+    {
+        _canFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + new Vector3(0,0.8f,0), Quaternion.identity);
     }
 }
